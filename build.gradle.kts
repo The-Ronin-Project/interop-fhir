59947cb4154b67464d22f95192edaf098a67ddac
Josh Smith <43484632+arssycro@users.noreply.github.com>
2022-07-29 10:23:29 -0500

    INT-889: Remove FHIR Validator (#59)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,22 +10,13 @@ plugins {
 dependencies {
     implementation(libs.interop.common)
     implementation(libs.interop.commonJackson)
-    implementation(libs.fhir.validation)
+
+    implementation(libs.jackson.core)
+    implementation(libs.jackson.databind)
 
     testImplementation(libs.jackson.module.kotlin)
     testImplementation(libs.mockk)
 
     // Used for some tests utilizing classpath inspection to verify configuration
     testImplementation(libs.classgraph)
-}
-
-tasks.withType<Test> {
-    minHeapSize = "2048m"
-    maxHeapSize = "4096m"
-}
-
-// For now, we will need to manually invoke the updateFHIRImplGuide gradle task.
-val updateFHIRImplGuide = task<Exec>("updateFHIRImplGuide") {
-    workingDir = projectDir
-    commandLine("./update_FHIR_impl_guide.sh")
 }
--- src/main/kotlin/com/projectronin/interop/fhir/validate/ValidationEntry.kt
+++
@@ -1,6 +0,0 @@
-package com.projectronin.interop.fhir.validate
-
-/**
- * A specific FHIR validation entry.
- */
-data class ValidationEntry(val severity: String, val location: String, val details: String)
