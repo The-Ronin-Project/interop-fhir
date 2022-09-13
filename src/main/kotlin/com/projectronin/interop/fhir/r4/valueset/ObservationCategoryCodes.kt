afbf1c942a0e5c7d363da76aab2a2104c3808f2a
Susan Korgen <susan@projectronin.com>
2022-09-13 15:15:01 -0400

    INT-1059 - this enum was overlooked previously (#88)
    * this enum was overlooked, values were used instead
    
    * clarify scope of list

---
+++ src/main/kotlin/com/projectronin/interop/fhir/r4/valueset/ObservationCategoryCodes.kt
@@ -0,0 +1,24 @@
+package com.projectronin.interop.fhir.r4.valueset
+
+import com.fasterxml.jackson.annotation.JsonValue
+import com.projectronin.interop.common.enums.CodedEnum
+
+/**
+ * See [US Core](https://www.hl7.org/fhir/us/core/ValueSet-us-core-observation-category.html),
+ * comprising [FHIR Spec](https://hl7.org/fhir/R4/valueset-observation-category.html),
+ * [US Core (clinical-test)](https://hl7.org/fhir/R4/valueset-observation-category.html), and
+ * [US Core (sdoh - social determinants of health)](https://www.hl7.org/fhir/us/core/CodeSystem-us-core-tags.html).
+ */
+enum class ObservationCategoryCodes constructor(@JsonValue override val code: String) : CodedEnum<ObservationCategoryCodes> {
+    SOCIAL_HISTORY("social-history"),
+    VITAL_SIGNS("vital-signs"),
+    IMAGING("imaging"),
+    LABORATORY("laboratory"),
+    PROCECURE("procedure"),
+    SURVEY("survey"),
+    EXAM("exam"),
+    THERAPY("therapy"),
+    ACTIVITY("activity"),
+    CLINICAL_TEST("clinical-test"),
+    SDOH("sdoh");
+}
---
+++ src/test/kotlin/com/projectronin/interop/fhir/r4/valueset/ObservationCategoryCodesTest.kt
@@ -0,0 +1,15 @@
+package com.projectronin.interop.fhir.r4.valueset
+
+import com.projectronin.interop.common.enums.CodedEnum
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class ObservationCategoryCodesTest {
+
+    @Test
+    fun `can find enums by code`() {
+        for (enum in ObservationCategoryCodes.values()) {
+            Assertions.assertEquals(enum, CodedEnum.byCode<ObservationCategoryCodes>(enum.code))
+        }
+    }
+}
