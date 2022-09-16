e1d68a4fc4dbe2e97b035bc9e7b465e97d5f5e00
Sam Felt <samuel.edward.felt@gmail.com>
2022-09-16 10:31:56 -0700

    [INT-1066] Move Reference utilities to interop-FHIR (#89)

--- src/test/kotlin/com/projectronin/interop/fhir/r4/datatype/ReferenceTest.kt
+++ src/test/kotlin/com/projectronin/interop/fhir/r4/datatype/ReferenceTest.kt
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.projectronin.interop.common.jackson.JacksonManager.Companion.objectMapper
 import com.projectronin.interop.fhir.r4.datatype.primitive.Uri
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class ReferenceTest {
@@ -72,5 +74,69 @@ class ReferenceTest {
         assertNull(reference.type)
         assertNull(reference.identifier)
         assertNull(reference.display)
+    }
+
+    @Test
+    fun `isForType - works when reference present`() {
+        val reference = Reference(reference = ("Patient/"))
+        assertTrue(reference.isForType("Patient"))
+        assertFalse(reference.isForType("Location"))
+    }
+    @Test
+    fun `isForType -  works when reference not present`() {
+        val reference = Reference(reference = ("HotGarbage!"))
+        assertFalse(reference.isForType("Patient"))
+    }
+
+    @Test
+    fun `isForType -  works when reference present but bad`() {
+        val reference = Reference()
+        assertFalse(reference.isForType("Patient"))
+    }
+
+    @Test
+    fun `decomposedID - works when id is present`() {
+        val reference = Reference(id = "123")
+        assertEquals("123", reference.decomposedId())
+    }
+
+    @Test
+    fun `decomposedID - works when no is present but reference is valid`() {
+        val reference = Reference(reference = ("Patient/123"))
+        assertEquals("123", reference.decomposedId())
+    }
+
+    @Test
+    fun `decomposedID - returns null when no id and reference has no ID`() {
+        val reference = Reference(reference = ("Patient/"))
+        assertNull(reference.decomposedId())
+    }
+    @Test
+    fun `decomposedID - returns null when no id and reference is bad`() {
+        val reference = Reference(reference = ("HotGarbage!"))
+        assertNull(reference.decomposedId())
+    }
+
+    @Test
+    fun `decomposedType - returns type when present`() {
+        val reference = Reference(type = Uri("Patient"))
+        assertEquals("Patient", reference.decomposedType())
+    }
+
+    @Test
+    fun `decomposedType - returns type when reference present`() {
+        val reference = Reference(reference = ("Patient/123"))
+        assertEquals("Patient", reference.decomposedType())
+    }
+    @Test
+    fun `decomposedType - returns null when reference present but no type`() {
+        val reference = Reference(reference = ("123"))
+        assertNull(reference.decomposedType())
+    }
+
+    @Test
+    fun `decomposedType - returns null when reference is bad`() {
+        val reference = Reference(reference = ("HotGarbage!"))
+        assertNull(reference.decomposedType())
     }
 }
