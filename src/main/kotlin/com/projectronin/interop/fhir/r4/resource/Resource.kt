2382b9ab9b0c1f45ae7b4d24e9e5e9d74b7950b5
Susan Korgen <susan@projectronin.com>
2023-02-01 15:56:07 -0500

    INT-1417 - Comment for maintaining vendor-specific list (#149)
    * Comment to assist vendor-specific maintenance
    
    * Update Resource.kt
    
    * Update Resource.kt

--- src/main/kotlin/com/projectronin/interop/fhir/r4/resource/Resource.kt
+++ src/main/kotlin/com/projectronin/interop/fhir/r4/resource/Resource.kt
@@ -26,6 +26,10 @@ import com.projectronin.interop.fhir.validate.Validatable
     defaultImpl = UnknownResource::class,
     visible = true
 )
+
+/**
+ * This is the list of FHIR resources that Ronin product code can use.
+ */
 @JsonSubTypes(
     JsonSubTypes.Type(Appointment::class),
     JsonSubTypes.Type(Bundle::class),
