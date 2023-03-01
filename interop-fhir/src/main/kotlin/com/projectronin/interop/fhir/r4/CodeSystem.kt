318ccb028be1877b41e051aad22927b6c485d676
azpr8 <108435629+azpr8@users.noreply.github.com>
2023-03-01 09:16:12 -0700

    alphabetized codesystems and added diagnostic report lab codesystem value (#164)

--- interop-fhir/src/main/kotlin/com/projectronin/interop/fhir/r4/CodeSystem.kt
+++ interop-fhir/src/main/kotlin/com/projectronin/interop/fhir/r4/CodeSystem.kt
@@ -6,20 +6,21 @@ import com.projectronin.interop.fhir.r4.datatype.primitive.Uri
  * Enumeration of common code systems used in FHIR.
  */
 enum class CodeSystem(uriString: String) {
-    UCUM("http://unitsofmeasure.org"),
+    CAREPLAN_CATEGORY("http://hl7.org/fhir/us/core/CodeSystem/careplan-category"),
+    CONDITION_CATEGORY("http://terminology.hl7.org/CodeSystem/condition-category"),
+    CONDITION_CLINICAL("http://terminology.hl7.org/CodeSystem/condition-clinical"),
     CURRENCY("urn:iso:std:iso:4217"),
+    DIAGNOSTIC_REPORT_LABORATORY("http://terminology.hl7.org/CodeSystem/v2-0074"),
+    HL7_IDENTIFIER_TYPE("http://terminology.hl7.org/CodeSystem/v2-0203"),
+    LOINC("http://loinc.org"),
     NPI("http://hl7.org/fhir/sid/us-npi"),
     OBSERVATION_CATEGORY("http://terminology.hl7.org/CodeSystem/observation-category"),
-    HL7_IDENTIFIER_TYPE("http://terminology.hl7.org/CodeSystem/v2-0203"),
-    CONDITION_CATEGORY("http://terminology.hl7.org/CodeSystem/condition-category"),
-    LOINC("http://loinc.org"),
-    CONDITION_CLINICAL("http://terminology.hl7.org/CodeSystem/condition-clinical"),
-    SNOMED_CT("http://snomed.info/sct"),
-    RXNORM("http://www.nlm.nih.gov/research/umls/rxnorm"),
-    CAREPLAN_CATEGORY("http://hl7.org/fhir/us/core/CodeSystem/careplan-category"),
     RONIN_TENANT("http://projectronin.com/id/tenantId"),
     RONIN_MRN("http://projectronin.com/id/mrn"),
-    RONIN_FHIR_ID("http://projectronin.com/id/fhir");
+    RONIN_FHIR_ID("http://projectronin.com/id/fhir"),
+    RXNORM("http://www.nlm.nih.gov/research/umls/rxnorm"),
+    SNOMED_CT("http://snomed.info/sct"),
+    UCUM("http://unitsofmeasure.org");
 
     val uri = Uri(uriString)
 }
