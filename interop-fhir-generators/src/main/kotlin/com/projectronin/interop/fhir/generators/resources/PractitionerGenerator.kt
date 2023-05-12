c946f154d39902802c366c8f0cb70c60aab1a7a6
Susan Korgen <susan@projectronin.com>
2023-05-12 18:34:53 -0400

    Add all R4 attributes to Practitioner (#198)

--- interop-fhir-generators/src/main/kotlin/com/projectronin/interop/fhir/generators/resources/PractitionerGenerator.kt
+++ interop-fhir-generators/src/main/kotlin/com/projectronin/interop/fhir/generators/resources/PractitionerGenerator.kt
@@ -1,18 +1,30 @@
 package com.projectronin.interop.fhir.generators.resources
 
+import com.projectronin.interop.fhir.generators.datatypes.CodeableConceptGenerator
 import com.projectronin.interop.fhir.generators.datatypes.ExtensionGenerator
 import com.projectronin.interop.fhir.generators.datatypes.HumanNameGenerator
 import com.projectronin.interop.fhir.generators.datatypes.IdentifierGenerator
+import com.projectronin.interop.fhir.generators.primitives.CodeGenerator
+import com.projectronin.interop.fhir.generators.primitives.FHIRStringDataGenerator
+import com.projectronin.interop.fhir.r4.datatype.Address
+import com.projectronin.interop.fhir.r4.datatype.CodeableConcept
+import com.projectronin.interop.fhir.r4.datatype.ContactPoint
 import com.projectronin.interop.fhir.r4.datatype.Extension
 import com.projectronin.interop.fhir.r4.datatype.HumanName
 import com.projectronin.interop.fhir.r4.datatype.Identifier
 import com.projectronin.interop.fhir.r4.datatype.Meta
 import com.projectronin.interop.fhir.r4.datatype.Narrative
+import com.projectronin.interop.fhir.r4.datatype.Period
+import com.projectronin.interop.fhir.r4.datatype.Reference
 import com.projectronin.interop.fhir.r4.datatype.primitive.Code
+import com.projectronin.interop.fhir.r4.datatype.primitive.Date
+import com.projectronin.interop.fhir.r4.datatype.primitive.FHIRBoolean
 import com.projectronin.interop.fhir.r4.datatype.primitive.Id
 import com.projectronin.interop.fhir.r4.datatype.primitive.Uri
 import com.projectronin.interop.fhir.r4.resource.ContainedResource
 import com.projectronin.interop.fhir.r4.resource.Practitioner
+import com.projectronin.interop.fhir.r4.resource.Qualification
+import com.projectronin.interop.fhir.r4.valueset.AdministrativeGender
 import com.projectronin.test.data.generator.DataGenerator
 import com.projectronin.test.data.generator.NullDataGenerator
 import com.projectronin.test.data.generator.collection.ListDataGenerator
@@ -27,7 +39,14 @@ data class PractitionerGenerator(
     override val extension: ListDataGenerator<Extension> = ListDataGenerator(0, ExtensionGenerator()),
     override val modifierExtension: ListDataGenerator<Extension> = ListDataGenerator(0, ExtensionGenerator()),
     val identifier: ListDataGenerator<Identifier> = ListDataGenerator(0, IdentifierGenerator()),
-    val name: ListDataGenerator<HumanName> = ListDataGenerator(1, HumanNameGenerator())
+    val active: DataGenerator<FHIRBoolean?> = NullDataGenerator(),
+    val name: ListDataGenerator<HumanName> = ListDataGenerator(1, HumanNameGenerator()),
+    val telecom: ListDataGenerator<ContactPoint?> = ListDataGenerator(0, NullDataGenerator()),
+    val address: ListDataGenerator<Address?> = ListDataGenerator(0, NullDataGenerator()),
+    val gender: CodeGenerator = CodeGenerator(AdministrativeGender::class),
+    val birthDate: DataGenerator<Date?> = NullDataGenerator(),
+    val qualification: ListDataGenerator<Qualification> = ListDataGenerator(0, QualificationGenerator()),
+    val communication: ListDataGenerator<CodeableConcept> = ListDataGenerator(0, CodeableConceptGenerator())
 ) : DomainResource<Practitioner> {
     override fun toFhir(): Practitioner =
         Practitioner(
@@ -40,12 +59,45 @@ data class PractitionerGenerator(
             extension = extension.generate(),
             modifierExtension = modifierExtension.generate(),
             identifier = identifier.generate(),
-            name = name.generate()
+            active = active.generate(),
+            name = name.generate(),
+            telecom = telecom.generate().filterNotNull(),
+            address = address.generate().filterNotNull(),
+            gender = gender.generate(),
+            birthDate = birthDate.generate(),
+            qualification = qualification.generate(),
+            communication = communication.generate()
         )
+}
+
+class QualificationGenerator : DataGenerator<Qualification>() {
+    val id: FHIRStringDataGenerator = FHIRStringDataGenerator()
+    val extension: ListDataGenerator<Extension> = ListDataGenerator(0, ExtensionGenerator())
+    val modifierExtension: ListDataGenerator<Extension> = ListDataGenerator(0, ExtensionGenerator())
+    val identifier: ListDataGenerator<Identifier> = ListDataGenerator(0, IdentifierGenerator())
+    val code: DataGenerator<CodeableConcept> = CodeableConceptGenerator()
+    val period: DataGenerator<Period?> = NullDataGenerator()
+    val issuer: DataGenerator<Reference?> = NullDataGenerator()
+
+    override fun generateInternal() = Qualification(
+        id = id.generate(),
+        extension = extension.generate(),
+        modifierExtension = modifierExtension.generate(),
+        identifier = identifier.generate(),
+        code = code.generate(),
+        period = period.generate(),
+        issuer = issuer.generate()
+    )
 }
 
 fun practitioner(block: PractitionerGenerator.() -> Unit): Practitioner {
     val practitioner = PractitionerGenerator()
     practitioner.apply(block)
     return practitioner.toFhir()
+}
+
+fun qualification(block: QualificationGenerator.() -> Unit): Qualification {
+    val qualification = QualificationGenerator()
+    qualification.apply(block)
+    return qualification.generate()
 }
--- interop-fhir-generators/src/test/kotlin/com/projectronin/interop/fhir/generators/resources/PractitionerGeneratorTest.kt
+++ interop-fhir-generators/src/test/kotlin/com/projectronin/interop/fhir/generators/resources/PractitionerGeneratorTest.kt
@@ -1,9 +1,22 @@
 package com.projectronin.interop.fhir.generators.resources
 
+import com.projectronin.interop.common.jackson.JacksonManager
+import com.projectronin.interop.fhir.generators.datatypes.address
+import com.projectronin.interop.fhir.generators.datatypes.codeableConcept
+import com.projectronin.interop.fhir.generators.datatypes.coding
+import com.projectronin.interop.fhir.generators.datatypes.contactPoint
 import com.projectronin.interop.fhir.generators.datatypes.identifier
 import com.projectronin.interop.fhir.generators.datatypes.name
+import com.projectronin.interop.fhir.generators.primitives.date
+import com.projectronin.interop.fhir.r4.datatype.primitive.Code
+import com.projectronin.interop.fhir.r4.datatype.primitive.FHIRBoolean
+import com.projectronin.interop.fhir.r4.datatype.primitive.FHIRString
 import com.projectronin.interop.fhir.r4.datatype.primitive.Id
+import com.projectronin.interop.fhir.r4.datatype.primitive.asFHIR
+import com.projectronin.interop.fhir.r4.valueset.ContactPointSystem
+import com.projectronin.interop.fhir.util.asCode
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -21,7 +34,16 @@ class PractitionerGeneratorTest {
         assertEquals(0, practitioner.extension.size)
         assertEquals(0, practitioner.modifierExtension.size)
         assertTrue(practitioner.identifier.isEmpty())
+        assertNull(practitioner.active)
         assertEquals(1, practitioner.name.size)
+        assertTrue(practitioner.name.first().given.first().toString().isNotEmpty())
+        assertTrue(practitioner.name.first().family?.value.toString().isNotEmpty())
+        assertEquals(0, practitioner.telecom.size)
+        assertEquals(0, practitioner.address.size)
+        assertNotNull(practitioner.gender)
+        assertNull(practitioner.birthDate)
+        assertEquals(0, practitioner.qualification.size)
+        assertEquals(0, practitioner.communication.size)
     }
 
     @Test
@@ -31,10 +53,103 @@ class PractitionerGeneratorTest {
             identifier of listOf(
                 identifier {}
             )
+            active of FHIRBoolean(true)
             name of listOf(name { family of "Felt" })
+            telecom of listOf(
+                contactPoint {
+                    system of ContactPointSystem.EMAIL.asCode()
+                    value of "doctor.com"
+                }
+            )
+            address of listOf(
+                address {
+                    city of "Kansas City"
+                    state of "KS"
+                }
+            )
+            // gender of "Very"
+            birthDate of date { year of 1990 }
+            qualification of listOf(
+                qualification {
+                    code of codeableConcept {
+                        coding of listOf(
+                            coding {
+                                code of "codeA"
+                                system of "systemA"
+                            }
+                        )
+                        text of "textA"
+                    }
+                }
+            )
+            communication of listOf(
+                codeableConcept {
+                    coding of listOf(
+                        coding {
+                            code of "codeB"
+                            system of "systemB"
+                        }
+                    )
+                    text of "textB"
+                }
+            )
         }
         assertEquals("id", practitioner.id?.value)
         assertEquals(1, practitioner.identifier.size)
+        assertTrue(practitioner.identifier.first().system?.value.toString().isNotEmpty())
+        assertTrue(practitioner.identifier.first().value?.value.toString().isNotEmpty())
+        assertEquals(true.asFHIR(), practitioner.active)
         assertEquals("Felt", practitioner.name.first().family?.value)
+        assertEquals(Code("email"), practitioner.telecom.first().system)
+        assertEquals(FHIRString("doctor.com"), practitioner.telecom.first().value)
+        assertEquals(FHIRString("Kansas City"), practitioner.address.first().city)
+        assertEquals(FHIRString("KS"), practitioner.address.first().state)
+        assertNotNull(practitioner.gender)
+        assertTrue(practitioner.birthDate?.value?.startsWith("1990")!!)
+        assertEquals("systemA", practitioner.qualification.first().code?.coding?.first()?.system?.value)
+        assertEquals("codeA", practitioner.qualification.first().code?.coding?.first()?.code?.value)
+        assertEquals("textA", practitioner.qualification.first().code?.text?.value)
+        assertEquals("systemB", practitioner.communication.first().coding.first().system?.value)
+        assertEquals("codeB", practitioner.communication.first().coding.first().code?.value)
+        assertEquals("textB", practitioner.communication.first().text?.value)
+    }
+
+    @Test
+    fun `example use with serialization`() {
+        // Create a practitioner with the details you need.
+        val practitioner = practitioner {
+            id of Id("id")
+            identifier of listOf(
+                identifier {}
+            )
+            name of listOf(name { family of "Felt" })
+            gender of "Very"
+            birthDate of date { year of 1990 }
+
+            // Say your test require a qualification
+            qualification of listOf(
+                qualification {
+                    code of codeableConcept {
+                        coding of listOf(
+                            coding {
+                                code of "codeA"
+                                system of "systemA"
+                            }
+                        )
+                        text of "textA"
+                    }
+                }
+            )
+
+            // And active status
+            active of FHIRBoolean(true)
+        }
+
+        // This object can be serialized to JSON to be injected into your workflow, all specified R4 attributes wil be generated
+        val practitionerJSON = JacksonManager.objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(practitioner)
+
+        // Uncomment to take a peek at the JSON
+        // println(practitionerJSON)
+        assertNotNull(practitionerJSON)
     }
 }
