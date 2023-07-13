f2382ffa6b5d68986c229e0bfe609e522f8504bf
douglas-projectronin <123015430+douglas-projectronin@users.noreply.github.com>
2023-07-13 10:33:50 -0400

    INT-1893 Upgraded interop-fhir to new dependency plugin and GHA (#229)
    * INT-1893 Upgraded interop-fhir to new dependency plugin and GHA
    
    * INT-1893 Updating version pinning to use @pin sytanx

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
-    id("com.projectronin.interop.gradle.publish") apply false
-    id("com.projectronin.interop.gradle.spring") apply false
-    id("com.projectronin.interop.gradle.version")
+    alias(libs.plugins.interop.gradle.publish) apply false
+    alias(libs.plugins.interop.gradle.spring) apply false
+    alias(libs.plugins.interop.gradle.version)
+    alias(libs.plugins.interop.gradle.junit) apply false
+    alias(libs.plugins.interop.version.catalog)
 }
 
 subprojects {
     apply(plugin = "com.projectronin.interop.gradle.publish")
-
-    // Disable releases hub from running on the subprojects. Main project will handle it all.
-    tasks.filter { it.group.equals("releases hub", ignoreCase = true) }.forEach { it.enabled = false }
 }
