ef8907385deb1fe3d53d3d145c0f339ff84c37a5
Steven Juana <50198085+StevenJuana@users.noreply.github.com>
2024-02-14 13:09:34 -0800

    INT-2561 Update Diagnostic Report Generator (#295)
    Co-authored-by: Steven Juana <stevenjuana@Ronin-282.local>

--- interop-fhir-generators/src/main/kotlin/com/projectronin/interop/fhir/generators/resources/DiagnosticReportGenerator.kt
+++ interop-fhir-generators/src/main/kotlin/com/projectronin/interop/fhir/generators/resources/DiagnosticReportGenerator.kt
@@ -1,20 +1,28 @@
 package com.projectronin.interop.fhir.generators.resources
 
+import com.projectronin.interop.fhir.generators.datatypes.AttachmentGenerator
 import com.projectronin.interop.fhir.generators.datatypes.CodeableConceptGenerator
+import com.projectronin.interop.fhir.generators.datatypes.DynamicValues
 import com.projectronin.interop.fhir.generators.datatypes.ExtensionGenerator
 import com.projectronin.interop.fhir.generators.datatypes.IdentifierGenerator
+import com.projectronin.interop.fhir.generators.datatypes.PeriodGenerator
 import com.projectronin.interop.fhir.generators.datatypes.ReferenceGenerator
 import com.projectronin.interop.fhir.generators.primitives.CodeGenerator
+import com.projectronin.interop.fhir.r4.datatype.Attachment
 import com.projectronin.interop.fhir.r4.datatype.CodeableConcept
+import com.projectronin.interop.fhir.r4.datatype.DynamicValue
 import com.projectronin.interop.fhir.r4.datatype.Extension
 import com.projectronin.interop.fhir.r4.datatype.Identifier
 import com.projectronin.interop.fhir.r4.datatype.Meta
 import com.projectronin.interop.fhir.r4.datatype.Narrative
 import com.projectronin.interop.fhir.r4.datatype.Reference
 import com.projectronin.interop.fhir.r4.datatype.primitive.Code
+import com.projectronin.interop.fhir.r4.datatype.primitive.FHIRString
 import com.projectronin.interop.fhir.r4.datatype.primitive.Id
+import com.projectronin.interop.fhir.r4.datatype.primitive.Instant
 import com.projectronin.interop.fhir.r4.datatype.primitive.Uri
 import com.projectronin.interop.fhir.r4.resource.DiagnosticReport
+import com.projectronin.interop.fhir.r4.resource.DiagnosticReportMedia
 import com.projectronin.interop.fhir.r4.resource.Resource
 import com.projectronin.test.data.generator.DataGenerator
 import com.projectronin.test.data.generator.NullDataGenerator
@@ -34,6 +42,18 @@ data class DiagnosticReportGenerator(
     val code: DataGenerator<CodeableConcept> = CodeableConceptGenerator(),
     val subject: DataGenerator<Reference> = ReferenceGenerator(),
     val category: ListDataGenerator<CodeableConcept> = ListDataGenerator(0, CodeableConceptGenerator()),
+    val encounter: DataGenerator<Reference> = ReferenceGenerator(),
+    val effective: DataGenerator<DynamicValue<Any>> = DiagnosticReportEffectiveGenerator(),
+    val issued: DataGenerator<Instant?> = NullDataGenerator(),
+    val performer: ListDataGenerator<Reference> = ListDataGenerator(0, ReferenceGenerator()),
+    val resultsInterpreter: ListDataGenerator<Reference> = ListDataGenerator(0, ReferenceGenerator()),
+    val specimen: ListDataGenerator<Reference> = ListDataGenerator(0, ReferenceGenerator()),
+    val result: ListDataGenerator<Reference> = ListDataGenerator(0, ReferenceGenerator()),
+    val imagingStudy: ListDataGenerator<Reference> = ListDataGenerator(0, ReferenceGenerator()),
+    val media: ListDataGenerator<DiagnosticReportMedia> = ListDataGenerator(0, DiagnosticReportMediaGenerator()),
+    val conclusion: DataGenerator<FHIRString?> = NullDataGenerator(),
+    val conclusionCode: ListDataGenerator<CodeableConcept> = ListDataGenerator(0, CodeableConceptGenerator()),
+    val presentedForm: ListDataGenerator<Attachment> = ListDataGenerator(0, AttachmentGenerator()),
 ) : DomainResource<DiagnosticReport> {
     override fun toFhir(): DiagnosticReport =
         DiagnosticReport(
@@ -50,7 +70,42 @@ data class DiagnosticReportGenerator(
             code = code.generate(),
             subject = subject.generate(),
             category = category.generate(),
+            encounter = encounter.generate(),
+            effective = effective.generate(),
+            issued = issued.generate(),
+            performer = performer.generate(),
+            resultsInterpreter = resultsInterpreter.generate(),
+            specimen = specimen.generate(),
+            result = result.generate(),
+            imagingStudy = imagingStudy.generate(),
+            media = media.generate(),
+            conclusion = conclusion.generate(),
+            conclusionCode = conclusionCode.generate(),
+            presentedForm = presentedForm.generate(),
         )
+}
+
+class DiagnosticReportMediaGenerator : DataGenerator<DiagnosticReportMedia>() {
+    val id: DataGenerator<FHIRString?> = NullDataGenerator()
+    val extension: ListDataGenerator<Extension> = ListDataGenerator(0, ExtensionGenerator())
+    val modifierExtension: ListDataGenerator<Extension> = ListDataGenerator(0, ExtensionGenerator())
+    val comment: DataGenerator<FHIRString?> = NullDataGenerator()
+    val link: DataGenerator<Reference> = ReferenceGenerator()
+
+    override fun generateInternal() =
+        DiagnosticReportMedia(
+            id = id.generate(),
+            extension = extension.generate(),
+            modifierExtension = modifierExtension.generate(),
+            comment = comment.generate(),
+            link = link.generate(),
+        )
+}
+
+class DiagnosticReportEffectiveGenerator : DataGenerator<DynamicValue<Any>>() {
+    override fun generateInternal(): DynamicValue<Any> {
+        return DynamicValues.period(PeriodGenerator().generate())
+    }
 }
 
 fun diagnosticReport(block: DiagnosticReportGenerator.() -> Unit): DiagnosticReport {
--- interop-fhir-generators/src/test/kotlin/com/projectronin/interop/fhir/generators/resources/DiagnosticReportGeneratorTest.kt
+++ interop-fhir-generators/src/test/kotlin/com/projectronin/interop/fhir/generators/resources/DiagnosticReportGeneratorTest.kt
@@ -1,10 +1,19 @@
 package com.projectronin.interop.fhir.generators.resources
 
+import com.projectronin.interop.fhir.generators.datatypes.DynamicValues
 import com.projectronin.interop.fhir.generators.datatypes.codeableConcept
 import com.projectronin.interop.fhir.generators.datatypes.identifier
 import com.projectronin.interop.fhir.generators.datatypes.reference
 import com.projectronin.interop.fhir.generators.primitives.of
+import com.projectronin.interop.fhir.r4.datatype.Attachment
+import com.projectronin.interop.fhir.r4.datatype.CodeableConcept
+import com.projectronin.interop.fhir.r4.datatype.DynamicValueType
+import com.projectronin.interop.fhir.r4.datatype.Period
+import com.projectronin.interop.fhir.r4.datatype.Reference
+import com.projectronin.interop.fhir.r4.datatype.primitive.FHIRString
 import com.projectronin.interop.fhir.r4.datatype.primitive.Id
+import com.projectronin.interop.fhir.r4.datatype.primitive.Instant
+import com.projectronin.interop.fhir.r4.resource.DiagnosticReportMedia
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
@@ -28,6 +37,18 @@ class DiagnosticReportGeneratorTest {
         assertNotNull(diagnosticReport.code)
         assertNotNull(diagnosticReport.subject)
         assertTrue(diagnosticReport.category.isEmpty())
+        assertNotNull(diagnosticReport.encounter)
+        assertNotNull(diagnosticReport.effective)
+        assertNull(diagnosticReport.issued)
+        assertEquals(0, diagnosticReport.performer.size)
+        assertEquals(0, diagnosticReport.resultsInterpreter.size)
+        assertEquals(0, diagnosticReport.specimen.size)
+        assertEquals(0, diagnosticReport.result.size)
+        assertEquals(0, diagnosticReport.imagingStudy.size)
+        assertEquals(0, diagnosticReport.media.size)
+        assertNull(diagnosticReport.conclusion)
+        assertEquals(0, diagnosticReport.conclusionCode.size)
+        assertEquals(0, diagnosticReport.presentedForm.size)
     }
 
     @Test
@@ -46,6 +67,18 @@ class DiagnosticReportGeneratorTest {
                     }
                 subject of reference("Patient", "123")
                 category of listOf(codeableConcept {})
+                encounter of reference("Patient", "234")
+                effective of DynamicValues.period(Period(FHIRString("Period")))
+                issued of Instant("Issued")
+                performer of listOf(reference("Patient", "345"))
+                resultsInterpreter of listOf(reference("Patient", "456"))
+                specimen of listOf(reference("Patient", "567"))
+                result of listOf(reference("Patient", "678"))
+                imagingStudy of listOf(reference("Patient", "789"))
+                media of listOf(DiagnosticReportMedia(link = Reference(FHIRString("Link"))))
+                conclusion of FHIRString("Conclusion")
+                conclusionCode of listOf(CodeableConcept(FHIRString("Conclusion Code")))
+                presentedForm of listOf(Attachment(FHIRString("Presented Form")))
             }
         assertEquals("id", diagnosticReport.id?.value)
         assertEquals(1, diagnosticReport.identifier.size)
@@ -53,5 +86,18 @@ class DiagnosticReportGeneratorTest {
         assertEquals("code", diagnosticReport.code?.text?.value)
         assertEquals("Patient/123", diagnosticReport.subject?.reference?.value)
         assertEquals(1, diagnosticReport.category.size)
+        assertEquals("Patient/234", diagnosticReport.encounter?.reference?.value)
+        assertEquals(DynamicValueType.PERIOD, diagnosticReport.effective?.type)
+        assertEquals(Period(FHIRString("Period")), diagnosticReport.effective?.value)
+        assertEquals("Issued", diagnosticReport.issued?.value)
+        assertEquals("Patient/345", diagnosticReport.performer.first().reference?.value)
+        assertEquals("Patient/456", diagnosticReport.resultsInterpreter.first().reference?.value)
+        assertEquals("Patient/567", diagnosticReport.specimen.first().reference?.value)
+        assertEquals("Patient/678", diagnosticReport.result.first().reference?.value)
+        assertEquals("Patient/789", diagnosticReport.imagingStudy.first().reference?.value)
+        assertEquals("Link", diagnosticReport.media.first().link?.id?.value)
+        assertEquals("Conclusion", diagnosticReport.conclusion?.value)
+        assertEquals("Conclusion Code", diagnosticReport.conclusionCode.first().id?.value)
+        assertEquals("Presented Form", diagnosticReport.presentedForm.first().id?.value)
     }
 }
